228fc58b4b506845a9d96b7cd32037d0cb679c22
Jingoro <jingoro@jingoro.net>
2020-08-01 01:30:03 -0400

    initial commit

---
+++ src/page/app/app.tsx
@@ -0,0 +1,38 @@
+
+/*
+  Calliope - Video Submission and Contest Management
+  Copyright © 2020 The Organizaion of Video Artists
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see https://www.gnu.org/licenses/.
+*/
+import React from 'react';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
